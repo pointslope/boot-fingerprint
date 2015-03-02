31e48273f56785fac9ac213a6584456a03d909a5
Daniel De Aguiar <ddeaguiar@pointslope.com>
2015-03-01 21:39:22 -0500

    Better documentation.

--- src/pointslope/boot_fingerprint.clj
+++ src/pointslope/boot_fingerprint.clj
@@ -6,11 +6,17 @@
             [boot.util :as util :refer [info]]))
 
 (defn fingerprint-pod
+  "Creates a pod with the requisite fingerprint dependencies."
   []
   (make-pod (update-in (get-env) [:dependencies] into '[[enlive "1.1.5"]
                                                         [pandect "0.5.1"]])))
 
 (deftask fingerprint
+  "Add cache buster fingerprints to all html files. Resources to be
+   fingerprinted should be wrapped in enlive's replace-var syntax.
+
+   <link rel=\"stylesheet\" src=\"${css/main.css}\" />
+"
   []
   (let [output-dir (temp-dir!)
         output-dir-path (.getPath output-dir)]
@@ -20,9 +26,12 @@
       (let [boot-tmp-files (by-ext [".html"] (user-files fileset))
             f-pod (fingerprint-pod)]
         (doseq [boot-tmp-file boot-tmp-files]
-          (pod/with-call-in f-pod
-            (pointslope.boot-fingerprint.fingerprint/fingerprint-file ~output-dir-path ~(.getPath (tmpfile boot-tmp-file)) ~(tmppath boot-tmp-file))
-            )))
+          (let [absolute-file-path (.getPath (tmpfile boot-tmp-file))
+                rel-file-path (tmppath boot-tmp-file)]
+            (pod/with-call-in f-pod
+              (pointslope.boot-fingerprint.fingerprint/fingerprint-file ~output-dir-path
+                                                                        ~absolute-file-path
+                                                                        ~rel-file-path)))))
       (-> fileset
           (add-resource output-dir)
           (commit!)))))
--- src/pointslope/boot_fingerprint/fingerprint.clj
+++ src/pointslope/boot_fingerprint/fingerprint.clj
@@ -4,16 +4,24 @@
             [clojure.java.io :as io]
             [pandect.algo.sha1 :refer [sha1-file]]))
 
-(defn fingerprint-asset [asset input-dir]
+(defn fingerprint-asset
+  "Returns a fingerprint based on the sha1 of the asset file, 'asset', located in the 
+   input dir, 'input-dir'."
+  [asset input-dir]
   (let [path (subs (str asset) 1)
         sha1 (sha1-file (str input-dir "/" path))
         fingerprint (str path "?v=" sha1)]
     (info (format "Adding fingerprint '%s'.\n" fingerprint))
     fingerprint))
 
-(defn fingerprint-file [output-dir file-path rel-path]
-  (let [root-input-dir (first (clojure.string/split file-path (re-pattern rel-path)))
-        input-file (io/file file-path)
+(defn fingerprint-file
+  "Adds a fingerprint query parameter to all asset vars in the file specified by the 'path'
+  parameter and creates the output file in the output directory, 'output-dir' with the specified
+  relative path, 'rel-path'. Nested output directories are created if necessary."
+  [output-dir path rel-path]
+  (let [root-input-dir (first (clojure.string/split path
+                                                    (re-pattern rel-path)))
+        input-file (io/file path)
         output-file (io/file output-dir rel-path)
         template-fn (template
                      (html-resource input-file)
